98ade06f4dfa19d36adb7a956b946f43079c35a0
tomasrzdevgold <tomasmolly@hotmail.com>
2023-08-20 23:49:38 -0300

    consegui rodar ultimo exercicio

--- index.js
+++ index.js
@@ -40,13 +40,13 @@ const idade = prompt("qual é sua idade")
 const idadeAmigo = prompt("qual é a idade de seu melhor amigo(a)")
 
 let comparacaoIdades = Number(idade) > Number(idadeAmigo)
-console.log("sua idade é maior do que a do seu melhor amigo?",comparacaoIdades)
+console.log("sua idade é maior do que a do seu melhor amigo?", comparacaoIdades)
 console.log(Number(idade) - Number(idadeAmigo))
 
 //exercicio 2 codigo
 const numero = prompt("introduça um numero par")
 const numeroPar = Number(numero)
-console.log(numeroPar%2)
+console.log(numeroPar % 2)
 //com uns numeros pares da 0
 //com uns numeros impar da 1
 
@@ -54,32 +54,40 @@ console.log(numeroPar%2)
 const ano = prompt("sua idade")
 const ano2 = Number(ano)
 
-console.log("sua idade em meses é",ano2*12)
-console.log("sua idade em dias é", ano2*365)
+console.log("sua idade em meses é", ano2 * 12)
+console.log("sua idade em dias é", ano2 * 365)
 
 const horas = 365 * 24
 
-console.log("sua idade em horas é",ano2*horas)
+console.log("sua idade em horas é", ano2 * horas)
 
 
 //exercicio 4
 const primeiroNumero = prompt("introduça o primeiro numero")
 const segundoNumero = prompt("introduça o segundo numero")
 
+
+
+console.log("primeiro numero", primeiroNumero)
+console.log("segundo numero", segundoNumero)
+
 const primeiroNumeroN = Number(primeiroNumero)
 const segundoNumeroN = Number(segundoNumero)
 
-console.log("o primeiro numero é maior que segundo?",primeiroNumeroN > segundoNumeroN)
-console.log("o primeiro numero é igual ao segundo?",primeiroNumeroN===segundoNumeroN)
+console.log("o primeiro numero é maior que segundo?", primeiroNumeroN > segundoNumeroN)
+console.log("o primeiro numero é igual ao segundo?", primeiroNumeroN === segundoNumeroN)
 const div1 = primeiroNumeroN % segundoNumeroN
 const div2 = segundoNumeroN % primeiroNumeroN
 
-const restDiv1 = div1 === 0 
+
+
+const restDiv1 = div1 === 0
 let restDiv2 = div2 >= 1
 
-// restDiv2 = !restDiv2 
+restDiv2 = !restDiv2
 
-console.log("o primeiro numero é divisivel pelo segundo?",restDiv1 )
-console.log("o segundo numero é divisivel pelo primeiro?",restDiv2)
-console.log(div2)
+console.log("o primeiro numero é divisivel pelo segundo?", restDiv1)
+console.log("o segundo numero é divisivel pelo primeiro?", restDiv2)
+
 console.log(div1)
+console.log(div2)
